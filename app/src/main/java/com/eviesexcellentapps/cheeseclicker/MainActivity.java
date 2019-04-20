58d05862fc4d70b20ff601adf96351b1a829ed8c
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-04-20 10:31:37 +0100

    Added toast to appear onclick. Changed the label that will appear at the top navbar to Cheese Clicker with a space

--- app/src/main/java/com/eviesexcellentapps/cheeseclicker/MainActivity.java
+++ app/src/main/java/com/eviesexcellentapps/cheeseclicker/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +17,12 @@ public class MainActivity extends AppCompatActivity {
 //        set up the var that will be the cheese
         ImageView cheese = findViewById(R.id.imageCheese);
 
-        // create onclick listener for the cheese
+        // create onclick listener for the cheese and tell Java what to do when the cheese gets clicked
         cheese.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(MainActivity.this, "hello!", Toast.LENGTH_SHORT).show();
             }
-                                  }
-
-
-        );
+        });
     }
 }
