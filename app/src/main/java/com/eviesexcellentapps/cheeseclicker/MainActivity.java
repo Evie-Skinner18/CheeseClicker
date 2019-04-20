9f71f3be0fe1facd0fa8d49e0a168c12c9a8f8d1
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-04-20 10:25:43 +0100

    Learning some Java by writing the onClick method for the cheese

--- app/src/main/java/com/eviesexcellentapps/cheeseclicker/MainActivity.java
+++ app/src/main/java/com/eviesexcellentapps/cheeseclicker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.eviesexcellentapps.cheeseclicker;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        set up the var that will be the cheese
+        ImageView cheese = findViewById(R.id.imageCheese);
+
+        // create onclick listener for the cheese
+        cheese.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+                                  }
+
+
+        );
     }
 }
