d6a07a382b448a8ba4f9efe5829b5df9a16bf01d
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-04-20 10:57:09 +0100

    Changed the Java so the currentScore var will be rendered in the score text instead of a toast

--- app/src/main/java/com/eviesexcellentapps/cheeseclicker/MainActivity.java
+++ app/src/main/java/com/eviesexcellentapps/cheeseclicker/MainActivity.java
@@ -5,23 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int currentScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        set up the var that will be the cheese
+//        set up the var that will be the cheese and also the var for the score rendered in the xml
         ImageView cheese = findViewById(R.id.imageCheese);
+        final TextView textViewScore = findViewById(R.id.score);
 
         // create onclick listener for the cheese and tell Java what to do when the cheese gets clicked
         cheese.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "hello!", Toast.LENGTH_SHORT).show();
+                currentScore++;
+//                Toast.makeText(MainActivity.this, "Your score is now " + currentScore, Toast.LENGTH_SHORT).show();
+                textViewScore.setText(String.valueOf(currentScore));
             }
         });
     }
