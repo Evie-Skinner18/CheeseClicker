d56fd81f86bc9adb9f84fdec14438082436e368d
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-04-25 20:28:17 +0100

    Added timer and button with ma bro Danny at codebar

--- app/src/main/java/com/eviesexcellentapps/cheeseclicker/MainActivity.java
+++ app/src/main/java/com/eviesexcellentapps/cheeseclicker/MainActivity.java
@@ -1,30 +1,42 @@
 package com.eviesexcellentapps.cheeseclicker;
 
 import android.content.DialogInterface;
+import android.os.CountDownTimer;
+import android.preference.EditTextPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.BreakIterator;
+
 public class MainActivity extends AppCompatActivity {
 
     private int currentScore = 0;
+    private TextView timerText;
+    private CountDownTimer countDownTimer;
+    private boolean isRunning = false;
+    private boolean hasTimerCompleted = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-//        set up the var that will be the cheese and also the var for the score rendered in the xml
+    protected void initResources() {
+        //        set up the var that will be the cheese and also the var for the score rendered in the xml
         ImageView cheese = findViewById(R.id.imageCheese);
         final TextView textViewScore = findViewById(R.id.score);
+        timerText = findViewById(R.id.timer);
 
         // create onclick listener for the cheese and tell Java what to do when the cheese gets clicked
         cheese.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+
+                if (!isRunning && hasTimerCompleted)
+                {
+                    return;
+                }
+
                 currentScore++;
                 textViewScore.setText(String.valueOf(currentScore));
 
@@ -38,5 +50,51 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        Button button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+
+                                          countDownTimer.start();
+                                          isRunning = true;
+
+                                      }
+                                  }
+
+        );
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        countDownTimer = new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                if (isRunning)
+                {
+                    timerText.setText("seconds remaining: " + millisUntilFinished / 1000);
+                }
+            }
+
+            public void onFinish() {
+                timerText.setText("done!");
+                isRunning = false;
+                hasTimerCompleted = true;
+
+            }
+        };
+
+        // separate logic in onCreate from the creation of resources
+                initResources();
+
+
+
     }
 }
